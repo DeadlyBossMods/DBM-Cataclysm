1c1c529917afe7dfafea959f4223bfdade3adc17
tandanu <tandanu@1bf76c0e-bbb0-4f41-aeb0-31569b98177e>
2013-03-27 22:07:34 +0000

    flag trash mods

--- trunk/DBM-BlackwingDescent/BWDTrash.lua
+++ trunk/DBM-BlackwingDescent/BWDTrash.lua
@@ -4,6 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetModelID(29539)
 mod:SetZone()
+mod.isTrashMod = true
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
