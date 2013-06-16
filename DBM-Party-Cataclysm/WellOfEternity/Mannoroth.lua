3530de98bc42b46cefefa8186ad4ec04254e92be
Adam <MysticalOS@users.noreply.github.com>
2013-06-16 04:14:08 +0000

    prevent bad recombat with Mannorath since he doesn't die on defeat and you can keep hitting him.

--- DBM-Party-Cataclysm/WellOfEternity/Mannoroth.lua
+++ DBM-Party-Cataclysm/WellOfEternity/Mannoroth.lua
@@ -4,6 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(54969)
 mod:SetModelID(38996)
+mod:SetReCombatTime(60)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
