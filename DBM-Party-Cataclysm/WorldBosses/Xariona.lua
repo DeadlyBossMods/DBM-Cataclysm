9678bb168f08f8f303ec022cbd81a800ef896792
Adam <MysticalOS@users.noreply.github.com>
2018-02-10 01:32:28 +0000

    Must use UNIT_POWER_FREQUENT, UNIT_POWER depricated

--- DBM-Party-Cataclysm/WorldBosses/Xariona.lua
+++ DBM-Party-Cataclysm/WorldBosses/Xariona.lua
@@ -13,7 +13,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
-	"UNIT_POWER",
+	"UNIT_POWER_FREQUENT",
 	"UNIT_SPELLCAST_SUCCEEDED"
 )
 mod.onlyNormal = true
@@ -85,7 +85,7 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
-function mod:UNIT_POWER(uId)
+function mod:UNIT_POWER_FREQUENT(uId)
 	if self:GetUnitCreatureId(uId) == 50061 and UnitPower(uId) == 70 and specialCharging then
 		warnUnleashedMagicSoon:Schedule(8)
 		timerUnleashedMagicCD:Start(18)--Start a bar in case one doesn't exist, so update function can do it's thing after.
