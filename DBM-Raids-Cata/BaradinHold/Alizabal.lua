cc31bfba4a71393fc2876d50cc23a950d302a723
Björn Heiß <b.boeing@online.de>
2025-02-26 23:02:22 +0100

    Fix warning for Alizabals Seething Hate
    The DMG is split amongst all nearby players

--- DBM-Raids-Cata/BaradinHold/Alizabal.lua
+++ DBM-Raids-Cata/BaradinHold/Alizabal.lua
@@ -20,7 +20,7 @@ local warnSeethingHate			= mod:NewTargetAnnounce(105067, 3)
 
 local specWarnBladeDance		= mod:NewSpecialWarningRun(104995, nil, nil, nil, 4, 2)
 local specWarnSkewer			= mod:NewSpecialWarningTaunt(104936, nil, nil, nil, 1, 2)
-local specWarnSeethingHate		= mod:NewSpecialWarningMoveAway(105067, nil, nil, nil, 1, 2)
+local specWarnSeethingHate		= mod:NewSpecialWarningStack(105067, nil, nil, nil, 1, 2)
 
 local timerBladeDance			= mod:NewBuffActiveTimer(15, 104995, nil, nil, nil, 6)
 local timerBladeDanceCD			= mod:NewCDTimer(60, 104995, nil, nil, nil, 6)
