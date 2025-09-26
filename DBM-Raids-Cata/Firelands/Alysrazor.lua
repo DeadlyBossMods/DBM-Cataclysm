d2981fea8a33ccaae8a75e1790a956d2b89e4948
MysticalOS <mysticalosx@gmail.com>
2025-09-25 20:08:25 -0400

    improve intiitate detection with protection against mismatching cases

--- DBM-Raids-Cata/Firelands/Alysrazor.lua
+++ DBM-Raids-Cata/Firelands/Alysrazor.lua
@@ -188,6 +188,7 @@ end
 
 do
 	local initiate = DBM:EJ_GetSectionInfo(2834)
+	initiate = string.lower(initiate or "")
 	local intiateTimers = {27, 31, 31, 21, 21, 21}
 	local intiateHeroicTimers = {27, 22, 63, 21, 21, 40}
 	local initiateSpawns = {
@@ -205,7 +206,7 @@ do
 			timerMoltingCD:Cancel()
 			timerPhaseChange:Start(33, 3)
 			self.vb.initiatesSpawned = 0
-		elseif mob == initiate then--initiates yell when they spawn, and no other time
+		elseif mob and string.lower(mob) == initiate then--initiates yell when they spawn, and no other time
 			self.vb.initiatesSpawned = self.vb.initiatesSpawned + 1
 			warnNewInitiate:Show(initiateSpawns[self.vb.initiatesSpawned])
 			if self.vb.initiatesSpawned == 6 then return end--All 6 are spawned, lets not create any timers.
